b5baa853cb30b42620e9ff319aaf98e23702eba0
Ahmed Assal <ahmed.assal@vertex-techs.com>
2015-10-28 16:50:49 +0000

    Initial version

---
+++ dectrees-py/hw1.py
@@ -0,0 +1,9 @@
+import monkdata as m
+import dtree as d
+
+print("monk1 entropy: ", d.entropy(m.monk1))
+print("monk1 entropy: ",d.entropy(m.monk1test))
+print("monk2 entropy: ",d.entropy(m.monk2))
+print("monk2test entropy: ",d.entropy(m.monk2test))
+print("monk3 entropy: ",d.entropy(m.monk3))
+print("monk3test entropy: ",d.entropy(m.monk3test))
